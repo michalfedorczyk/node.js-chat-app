5f91f0599350c933d6077201068edcbdee84c4c3
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-12 21:28:38 +0200

    Project structure to use socket.io

--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
-const app= require('./app')
-const port = process.env.port
+const path = require('path')
+const http= require('http')
+const express = require ('express')
+const socketio= require('socket.io')
 
-app.listen(port, ()=>{
-    console.log('Server started at port '+port)
+const port = process.env.PORT
+
+const app= express()
+const server= http.createServer(app)
+const io= socketio(server)
+
+const publicDirectoryPath= path.join(__dirname, '../public')
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', ()=>{
+    console.log('New connection')
+})
+
+server.listen(port, () => {
+    console.log('Server started at port '+ port)
 })
