92f4646f3bb043fe9b64a50f2b728a60f8320a18
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-13 22:17:19 +0200

    Simple Socket IO connections betweene server and clients. Clients are able to share their location, message

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,29 @@
-io()
+const socket = io();
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+document.querySelector("#message-form").addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const message = e.target.elements.message.value;
+
+  socket.emit("sendMessage", message);
+});
+
+document.querySelector("#send-location").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const location = "Location: lat " + latitude + " , long: " + longitude;
+
+    socket.emit("sendLocation", { location, latitude, longitude });
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,21 +1,41 @@
-const path = require('path')
-const http= require('http')
-const express = require ('express')
-const socketio= require('socket.io')
+const http = require("http");
+const path = require("path");
+const express = require("express");
+const socketio = require("socket.io");
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-const app= express()
-const server= http.createServer(app)
-const io= socketio(server)
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-const publicDirectoryPath= path.join(__dirname, '../public')
-app.use(express.static(publicDirectoryPath))
+const publicDirectoryPath = path.join(__dirname, "../public");
+app.use(express.static(publicDirectoryPath));
 
-io.on('connection', ()=>{
-    console.log('New connection')
-})
+let count = 0;
+
+io.on("connection", (socket) => {
+  console.log("New WebSocket connection");
+
+  socket.emit("message", "Welcome!");
+  socket.broadcast.emit("message", "new user joined room");
+
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message);
+  });
+
+  socket.on("sendLocation", ({ location, latitude, longitude }) => {
+    socket.broadcast.emit(
+      "message",
+      location + " https://google.com/maps?q=" + latitude + "," + longitude
+    );
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left!");
+  });
+});
 
 server.listen(port, () => {
-    console.log('Server started at port '+ port)
-})
+  console.log("Server started at port " + port);
+});
