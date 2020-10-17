fc8439ac5fc7e45b628bae41cc1c94ddd0bd3992
Michal M. Fedorczyk <michalfedorczyk@gmail.com>
2020-10-17 14:58:00 +0200

    In that commit I realized nex couple lessons in the course. Right now I can see an error about line nr 51 in index.js line. It's all about calling the callback() at the end of socket.on (join) option

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const path = require("path");
+const favicon = require("serve-favicon");
 const express = require("express");
 const socketio = require("socket.io");
 const Filter = require("bad-words");
@@ -7,6 +8,12 @@ const {
   generateMessage,
   generateLocationMessage,
 } = require("./utils/messages");
+const {
+  addUser,
+  getUser,
+  removeUser,
+  getUsersInRoom,
+} = require("./utils/users");
 
 const port = process.env.PORT;
 
@@ -17,11 +24,32 @@ const io = socketio(server);
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+app.use(favicon(path.join(__dirname, "../public/img/favicon.png")));
+
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  socket.emit("message", generateMessage("Welcome"));
-  socket.broadcast.emit("message", generateMessage("New User joined!"));
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({
+      id: socket.id,
+      username,
+      room,
+    });
+
+    if (error) {
+      return callback(error);
+    }
+
+    socket.join(user.room);
+    socket.emit("message", generateMessage("Welcome"));
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        generateMessage(`${user.username} joined the ${user.room}`)
+      );
+    callback()  
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
@@ -46,7 +74,14 @@ io.on("connection", (socket) => {
   });
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("User has left!"));
+    const user = removeUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left!`)
+      );
+    }
   });
 });
 
